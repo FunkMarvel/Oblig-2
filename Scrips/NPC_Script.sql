2afa7d6d1ce3087189906bfdced44fc3775cbba4
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2022-10-29 13:31:21 +0200

    Update NPC_Script.sql

--- Scrips/NPC_Script.sql
+++ Scrips/NPC_Script.sql
@@ -1,4 +1,11 @@
 
 
 
---
+-- NPC VALUES
+
+INSERT INTO gatchaimpact.npc(Name, Type, Health, Mana, Difficulity) VALUES
+("Arne Hole", "Mathmagician", 1000, 9001, 9),
+("Harry Hole", "Proffesional Alcohol drinker", 50, 24, 1),
+("Georg", "King of jungle", 80, 15, 5),
+("John Arne Riise", "Football-player", 43, 10, 6),
+("Amouranth", "Water", 116, 45, 15);
