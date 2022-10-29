df984e04cc26a33498988457a4f592d9b81ab1ce
Mathias M <mathmoe64@gmail.com>
2022-10-29 12:47:43 +0200

    Update GatchaImpactScripts.sql

--- Scrips/GatchaImpactScripts.sql
+++ Scrips/GatchaImpactScripts.sql
@@ -7,9 +7,15 @@ VALUES ('Potteplante', 'Test123'),
 -- Check userCredentials table
 SELECT * FROM gatchaimpact.usercredentials;
 
+
+Insert into gatchaimpact.player(PlayerID, UserName, ScoreBoardID,Money, Mana,QuestCompleteness)
+VALUES (0, 'Binugs', 15, 1,1 , 1);
+
 Insert into gatchaimpact.player(UserName, ScoreBoardID,Money, Mana,QuestCompleteness)
-VALUES ('Darth Waker', 'awdaw', 15000, 100, 52),
-       ('Mark Blader' 'the good side', 150, 20, 1, 21);
+VALUES ('Darth Waker', 0, 15000, 100, 52),
+       ('Mark Blader', 1, 150, 20, 1, 21),
+       ('Park Ranger', 2, 4200, 1000, 75),
+       ('Ainz Flager', 3, 100000, 1500, 99);
 
 
 
