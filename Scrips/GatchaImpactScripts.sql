19bb4f297528d918ea95838d8ceaf0cf4ef35885
Mathias M <mathmoe64@gmail.com>
2022-10-29 12:58:46 +0200

    Update GatchaImpactScripts.sql

--- Scrips/GatchaImpactScripts.sql
+++ Scrips/GatchaImpactScripts.sql
@@ -7,16 +7,21 @@ VALUES ('Potteplante', 'Test123'),
 -- Check userCredentials table
 SELECT * FROM gatchaimpact.usercredentials;
 
-
-Insert into gatchaimpact.player(PlayerID, UserName, PlayerName, ScoreBoardID,Money, Mana,QuestCompleteness)
-VALUES (0, 'Potteplante', 'Mark Sukkerberg', 15, 1,1 , 1);
-
+-- Create example Players
 Insert into gatchaimpact.player(PlayerName, UserName, ScoreBoardID,Money, Mana,QuestCompleteness)
 VALUES ('Darth Waker', 'Potteplante', 0,  15000, 100, 52),
        ('Mark Blader','Space_Cat', 1, 150, 20, 1),
        ('Park Ranger', 'Potteplante', 2, 4200, 1000, 75),
-       ('Ainz Flager', 'Space_Cat', 3, 100000, 1500, 99);
+       ('Ainz Flager', 'Space_Cat', 3, 100000, 1500, 99),
+       ('Potteplante', 'Mark Sukkerberg', 15, 1,1 , 1);
 
+
+-- Create example Quests
+INSERT INTO gatchaimpact.quests(NPCID, PreviousQuest, NextQuest, QuestDescription, QuestRewardItemID, QuestRewardMoneyAmount)
+VALUES ()
+
+
+-- Create exampke ActiveQuests
 
 
 
