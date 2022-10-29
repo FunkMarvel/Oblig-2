8fbb62d0c40f6d82fc80fe847537e7a7a6fe663c
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2022-10-29 13:32:37 +0200

    Update NPC_Script.sql

--- Scrips/NPC_Script.sql
+++ Scrips/NPC_Script.sql
@@ -8,4 +8,6 @@ INSERT INTO gatchaimpact.npc(Name, Type, Health, Mana, Difficulity) VALUES
 ("Harry Hole", "Proffesional Alcohol drinker", 50, 24, 1),
 ("Georg", "King of jungle", 80, 15, 5),
 ("John Arne Riise", "Football-player", 43, 10, 6),
-("Amouranth", "Water", 116, 45, 15);
+("Amouranth", "Water", 116, 45, 15),
+("Bell Delphine", "Water", 114, 77, 10)
+;
