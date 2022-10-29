3a4103da0760935c2f101d8954610ed3f47756f6
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-10-29 13:28:57 +0200

    Update AndersRelatedScripts.sql

--- Scrips/AndersRelatedScripts.sql
+++ Scrips/AndersRelatedScripts.sql
@@ -4,13 +4,13 @@ INSERT INTO gatchaimpact.usercredentials
 VALUES ('Potteplante', 'Test123'),
        ('Space_Cat', 'BingusBangus');
 
--- Check userCredentials table
 SELECT * FROM gatchaimpact.usercredentials;
 
 -- Create example items
-INSERT INTO gatchaimpact.items(Type, Damage, Value)
-VALUES ('Stick', 5, 1),
-       ('Torch', 10, 5);
+INSERT INTO gatchaimpact.items(Name, Type, Damage, Value)
+VALUES ('Stick', 'One-handed', 5, 1),
+       ('Torch', 'One-handed', 10, 5),
+       ('Longsword', 'Versatile', 50, 45);
 
-
+SELECT * FROM gatchaimpact.items;
 
